95f576941dd5e1baabc666a165cb1ec0b6ebc224
frank-2001 <frankmakolongo@gmail.com>
2022-08-30 12:42:26 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+    <style>
+        body{
+            margin-top:30px;
+        }
+    </style>
+</head>
+<body>
+    <center>
+        <h2>Ir David</h2>
+    </center>
+</body>
+</html>
