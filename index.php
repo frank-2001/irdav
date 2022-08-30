a437c86fe5b8f812426da77bff8b5036b4c6b42b
frank-2001 <frankmakolongo@gmail.com>
2022-08-30 14:54:37 +0200

    tamplete

--- index.php
+++ index.php
@@ -1,19 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
-    <style>
-        body{
-            margin-top:30px;
-        }
-    </style>
-</head>
-<body>
-    <center>
-        <h2>Ir David</h2>
-    </center>
+
+<?php require "widget/head.php";   ?>
+
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+  <div class="wrapper">
+
+    <!-- Preloader -->
+    
+
+
+    <!-- Navbar -->
+    <?php 
+    if (!isset($_GET['profile'])) {
+       require "widget/preload.php";
+    }
+    if (!isset($_GET['connected'])) {
+      if (isset($_GET['new'])) {
+       require "widget/sign-up.php";
+      } else {
+       require "widget/sign-in.php";
+      }
+    } else {
+      require "widget/navbar.php"; 
+      if (isset($_GET['profile'])) {
+        require "widget/profile.php";
+      } else {
+       require "widget/body.php";
+      } 
+      require "widget/footer.php";
+    
+    
+    }
+    
+     
+    ?>
+
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- REQUIRED SCRIPTS -->
+  <!-- jQuery -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap -->
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- overlayScrollbars -->
+  <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/adminlte.js"></script>
+
+  <!-- PAGE PLUGINS -->
+  <!-- jQuery Mapael -->
+  <script src="plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+  <script src="plugins/raphael/raphael.min.js"></script>
+  <script src="plugins/jquery-mapael/jquery.mapael.min.js"></script>
+  <script src="plugins/jquery-mapael/maps/usa_states.min.js"></script>
+  <!-- ChartJS -->
+  <script src="plugins/chart.js/Chart.min.js"></script>
+
+  <!-- AdminLTE for demo purposes -->
+  <script src="dist/js/demo.js"></script>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="dist/js/pages/dashboard2.js"></script>
 </body>
+
 </html>
